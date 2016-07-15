67d14de3a404523e814d6340c495973cec38fcf5
davidrichardson <david.m.richardson@gmail.com>
2016-07-15 11:12:22 +0100

    No need to define constructor

--- src/main/java/uk/ac/ebi/subs/data/component/Contact.java
+++ src/main/java/uk/ac/ebi/subs/data/component/Contact.java
@@ -3,8 +3,6 @@ package uk.ac.ebi.subs.data.component;
 public class Contact {
     String emailAddress;
 
-    public Contact(){};
-
     public String getEmailAddress() {
         return emailAddress;
     }
