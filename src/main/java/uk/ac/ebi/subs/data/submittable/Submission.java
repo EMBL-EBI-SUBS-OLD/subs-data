87e00bf8a261292187a03bdf322bd16fd237adfb
David Richardson <davidr@ebi.ac.uk>
2016-08-19 13:30:57 +0100

    submission diagram

--- src/main/java/uk/ac/ebi/subs/data/submittable/Submission.java
+++ src/main/java/uk/ac/ebi/subs/data/submittable/Submission.java
@@ -5,12 +5,15 @@ import uk.ac.ebi.subs.data.component.Domain;
 import uk.ac.ebi.subs.data.component.Submitter;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Submission {
     List<Submittable> submissionItems = new ArrayList<Submittable>();
     Submitter submitter = new Submitter();
     Domain domain = new Domain();
+    Date submissionDate = new Date();
+    String status;
 
     public List<Submittable> getSubmissionItems() {
         return submissionItems;
@@ -34,5 +37,21 @@ public class Submission {
 
     public void setDomain(Domain domain) {
         this.domain = domain;
+    }
+
+    public Date getSubmissionDate() {
+        return submissionDate;
+    }
+
+    public void setSubmissionDate(Date submissionDate) {
+        this.submissionDate = submissionDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
