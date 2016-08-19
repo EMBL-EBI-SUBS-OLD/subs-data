27ea39ffde203868582723dcddc14574caa7e1d8
David Richardson <davidr@ebi.ac.uk>
2016-08-19 15:24:15 +0100

    add sample and core feature diagrams

--- src/main/java/uk/ac/ebi/subs/data/submittable/SampleGroup.java
+++ src/main/java/uk/ac/ebi/subs/data/submittable/SampleGroup.java
@@ -5,10 +5,12 @@ import uk.ac.ebi.subs.data.AbstractSubsEntity;
 import uk.ac.ebi.subs.data.component.SubsLink;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class SampleGroup extends AbstractSubsEntity {
     List<SubsLink> sampleRefs = new ArrayList<SubsLink>();
+    Date releaseDate = new Date();
 
     public List<SubsLink> getSampleRefs() {
         return sampleRefs;
@@ -16,5 +18,13 @@ public class SampleGroup extends AbstractSubsEntity {
 
     public void setSampleRefs(List<SubsLink> sampleRefs) {
         this.sampleRefs = sampleRefs;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
     }
 }
